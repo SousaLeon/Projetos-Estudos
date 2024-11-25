83ae763131df9a80914081a641e2642d8ef0eb0c
Mudestel <leonardosousas050815@gmail.com>
2024-11-24 22:00:58 -0300

    Calculadora em C, 1.0

--- Projetos C/Calculadora/main.c
+++ Projetos C/Calculadora/main.c
@@ -3,6 +3,25 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	return 0;
+void main() {
+	
+	int Num1, Num2, Resultado;
+	char operacao;
+	
+	printf("Digite o primeiro numero: ");
+	scanf("%d", &Num1);
+
+	printf("Informe o tipo de calculo (/, *, -, +): ");
+	scanf(" %c", &operacao);
+	
+	switch(operacao){
+		
+		case:
+			break;
+	}
+	
+	printf("Digite o segundo numero: ");
+	scanf("%d", &Num2);
+	
+	
 }
