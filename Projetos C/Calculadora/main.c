c845aa9cf0429441f4fd008831ae102c94181b26
Mudestel <leonardosousas050815@gmail.com>
2024-12-01 18:39:07 -0300

    Calculadora C: 1.2

--- Projetos C/Calculadora/main.c
+++ Projetos C/Calculadora/main.c
@@ -6,16 +6,19 @@
 void main() {
 	
 	int Num1, Num2, Resultado;
-	char operacao;
+	char operacao, opcao;
 	
-	printf("Digite o primeiro numero: ");
+	Inicio: 
+	system("cls"); 
+	
+	printf("\nDigite o primeiro numero: ");
 	scanf("%d", &Num1);
 
-	printf("Informe o tipo de calculo (/, *, -, +): ");
+	printf("Informe o tipo de calculo (/, X, -, +): ");
 	scanf(" %c", &operacao);
 		
 	printf("Digite o segundo numero: ");
-	scanf("%d", &Num2);
+	scanf(" %d", &Num2);
 	
 	switch(operacao){
 		
@@ -40,6 +43,20 @@ void main() {
 			Resultado = Num1 - Num2;
 			printf("O resultado e: %d", Resultado);
 			break;
+			
+		default:
+			printf("\nA operacao informada nao e valida ou apresenta caracteres incorretos! \n");
+			printf("\nDeseja tentar novamente? S/N: ");
+			scanf(" %c", &opcao);
+			
+			if(opcao == 'S' || opcao == 's'){
+				goto Inicio;
+			}
+	}	
+	printf("\nDeseja realizar outra operacao S/N ?");
+	scanf(" %c", &opcao);
+	if(opcao == 'S' || opcao == 's'){
+		goto Inicio;
 	}
 	
 }
